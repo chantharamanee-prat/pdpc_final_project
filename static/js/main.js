8e913c09d768617dd257ece4cec2a2651eb64b57
Prat <chantharamanee.prat@gmail.com>
2024-09-06 21:08:34 +0700

    chore: Remove unused code

--- static/js/main.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.select-all').addEventListener('click', function() {
-    const checkboxes = document.querySelectorAll('.category-card');
-    checkboxes.forEach(box => box.classList.add('selected'));
-});
